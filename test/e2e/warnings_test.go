7763573afa8d0c4b5fef93ae9239e086c2c8f977
Praveen Rewar <8457124+praveenrewar@users.noreply.github.com>
2021-09-27 22:20:40 +0530

    Update e2e test to make it more deterministic

--- test/e2e/warnings_test.go
+++ test/e2e/warnings_test.go
@@ -146,16 +146,7 @@ Succeeded`
 }
 
 func getServerMinorVersion() (minorVersion int, err error) {
-	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
-	configOverrides := &clientcmd.ConfigOverrides{}
-	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
-
-	config, err := kubeConfig.ClientConfig()
-	if err != nil {
-		return minorVersion, err
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
+	clientset, err := getKubernetesClientset()
 	if err != nil {
 		return minorVersion, err
 	}
@@ -170,4 +161,18 @@ func getServerMinorVersion() (minorVersion int, err error) {
 	}
 
 	return minorVersion, err
+}
+
+func getKubernetesClientset() (*kubernetes.Clientset, error) {
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	configOverrides := &clientcmd.ConfigOverrides{}
+	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+
+	config, err := kubeConfig.ClientConfig()
+	if err != nil {
+		return &kubernetes.Clientset{}, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	return clientset, err
 }
